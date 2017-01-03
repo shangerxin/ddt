cbbf981648633823fe10b1bac244bb0a101a86fa
erxin <shangerxin@hotmail.com>
2017-01-03 19:55:48 +0800

    Add test case for utils and complete set up dev environment for ATOM

--- source/global/utils.js
+++ source/global/utils.js
@@ -322,7 +322,7 @@ var utils = (function () {
         },
 
         getGUID(){
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            return 'xxxxxxxx-xxxx-8xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
                 var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
                 return v.toString(16);
             });
--- test/test-global/test-utils.js
+++ test/test-global/test-utils.js
@@ -1,3 +1,13 @@
 /**
  * Created by shange on 10/6/2016.
  */
+ var {sourcePath, expect} = require("../fixtures/global");
+ var {utils} = require(`${sourcePath}/global/utils`);
+
+ describe("test util", ()=>{
+   it("test get getGUID", ()=>{
+     let aGUID = utils.getGUID();
+     expect(aGUID).to.be.a("string");
+     expect(/\w{8}-\w{4}-8\w{3}-\w{4}-\w{12}/.test(aGUID)).to.be.true;
+   })
+ })
